159da3b886da1b3d0815e35e911a01233351d706
Vincent Laude <vincent.laude2@gmail.com>
2019-02-07 16:47:15 +0100

    Add socket.io and light chat server

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
 const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
 
 require('./db');
 
@@ -7,6 +12,15 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(process.env.PORT || 5000, () => {
+io.on('connection', (socket) => {
+    console.log('user connected');
+
+    socket.on('new-message', (message) => {
+        console.log(message);
+        io.emit('new-message', message);
+    });
+});
+
+server.listen(process.env.PORT || 3000, () => {
     console.log('🏋  Server listening on port 3000 ...');
 });
