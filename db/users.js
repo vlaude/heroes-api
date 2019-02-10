bc4d7524f36630bca3337d38e7685273f29eaa90
Vincent Laude <vincent.laude2@gmail.com>
2019-02-10 21:41:38 +0100

    Add email, hash and some stuff for model user

--- db/users.js
+++ db/users.js
@@ -1,19 +1,57 @@
+const bcrypt = require('bcrypt'); // https://github.com/kelektiv/node.bcrypt.js
+
 module.exports = (sequelize, DataTypes) => {
-  const Users = sequelize.define(
-    'Users',
-    {
-      username: DataTypes.STRING,
-    },
-    {
-      paranoid: true,
-    }
-  );
+    const Users = sequelize.define(
+        'Users',
+        {
+            id: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                primaryKey: true
+            },
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            email: {
+                type: DataTypes.STRING,
+                validate: {
+                    isEmail: true
+                }
+            },
+            hash: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                set(val) {
+                    const hash = bcrypt.hashSync(val, 12);
+                    this.setDataValue('hash', hash);
+                }
+            }
+        },
+        {
+            paranoid: true,
+            indexes: [
+                {
+                    unique: true,
+                    fields: ['username', 'email']
+                }
+            ]
+        }
+    );
 
-  // Users.sync({ force: true }).then(() => {
-  //   return Users.create({
-  //     username: 'admin',
-  //   });
-  // });
+    // we don't want to send password even if crypted
+    Users.excludeAttributes = ['hash'];
 
-  return Users;
+    Users.prototype.comparePassword = (password) => {
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, this.hash, (err, res) => {
+                if (err || !res) {
+                    return reject(new Error('INVALID CREDENTIALS'));
+                }
+                return resolve();
+            });
+        });
+    };
+
+    return Users;
 };
