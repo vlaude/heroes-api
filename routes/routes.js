851ea57099e3f9eb8492d8fd9947eb7d310d5747
Vincent Laude <vincent.laude2@gmail.com>
2019-05-06 18:51:48 +0200

    add github webhook handler

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const xhub = require('express-x-hub');
+
 const cors = require('cors');
 // const hpp = require('hpp');
 const helmet = require('helmet');
@@ -12,16 +14,43 @@ const apiMessages = require('./messages');
 const api = express();
 initAuth();
 
-api.use(express.json({ limit: '1mb' }));
 const apiRoutes = express.Router();
 
 api.use(bodyParser.json());
+api.use(bodyParser.urlencoded({ extended: true }));
+const xhubSecret = 'MySecretKey';
+api.use(xhub({ algorithm: 'sha1', secret: xhubSecret }));
+
 api.use(cors());
 // api.use(hpp);
 api.use(helmet());
 
 apiRoutes.get('/', (req, res) => {
   res.status(200).send({ message: 'Hello from the awesome heroes api !' });
+});
+
+apiRoutes.post('/webhook', function(req, res) {
+  if (!req.isXHubValid()) {
+    res.status(400).send('Invalid X-Hub Request');
+    console.log('Secret key is invalid');
+    return;
+  }
+
+  const command = req.headers['x-github-event'];
+  console.log('Command received : ' + command);
+
+  switch (command) {
+    case 'push':
+      console.log(req.body.sender.login);
+      console.log(req.body.sender.url);
+      console.log(req.body.head_commit.message);
+      res.send('Event push trigger');
+      break;
+
+    default:
+      res.status(400).send('Event not supported : ' + command);
+      console.log('Event not supported : ' + req.headers['X-Github-Event']);
+  }
 });
 
 apiRoutes
