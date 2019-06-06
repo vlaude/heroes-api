9a99ea695fbe9ebf00e39108a88007c978d5190a
Vincent Laude <vincent.laude2@gmail.com>
2019-06-06 16:52:11 +0200

    add connection to database with sequelize

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 
+const db = require('./db/models');
 const api = require('./routes/routes');
 const logger = require('./util/logger');
 
@@ -8,8 +9,15 @@ const server = http.Server(api);
 const port = process.env.PORT || 3000;
 const ip = process.env.IP || '0.0.0.0';
 
-server.listen(port, ip, err =>
-  err
-    ? logger.error(`Failed to start API : ${err.stack}`)
-    : logger.info(`Server is listening on port ${port} ...`)
-);
+db.sequelize
+  .authenticate()
+  .then(() => {
+    server.listen(port, ip, err =>
+      err
+        ? logger.error(`Failed to start API : ${err.stack}`)
+        : logger.info(`Server is listening on port ${port} ...`)
+    );
+  })
+  .catch(err => {
+    logger.error(`Failed to connect to database : ${err.stack}`);
+  });
