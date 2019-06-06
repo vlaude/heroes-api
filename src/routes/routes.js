404bf5706982b2ce22957649f4fbbaa65cdedaf6
Vincent Laude <vincent.laude2@gmail.com>
2019-06-06 11:48:21 +0200

    add pino and express-pino for logging

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,20 @@
 const express = require('express');
+// Pino logger for express api
+const pino = require('../util/logger');
 const bodyParser = require('body-parser');
 
-const cors = require('cors');
-const hpp = require('hpp');
+// Helmet helps you secure your Express apps by setting various HTTP headers
 const helmet = require('helmet');
+const cors = require('cors');
 
 const api = express();
 const apiRoutes = express.Router();
 
+api.use(pino);
 api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({ extended: true }));
-api.use(cors());
-// api.use(hpp);
 api.use(helmet());
+api.use(cors());
 
 apiRoutes.get('/', (req, res) => {
   res.status(200).send({ message: `Hello from my awesome API !` });
