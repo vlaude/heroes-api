2101e2f2c61024446b6120fafa46dea48d36254c
Vincent Laude <vincent.laude2@gmail.com>
2019-06-24 14:20:10 +0200

    rewrite user controller tests with auth

--- src/db/seeders/20190612115208-test-user.js
+++ src/db/seeders/20190612115208-test-user.js
@@ -2,6 +2,22 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
+        id: '09d10e5b-3f26-4e7b-9ecb-bdea62391259',
+        username: 'admin',
+        hash: '$2b$12$8k7GApCWSybxruBHy6WUbepyddIQjFX8h2Jqhboa61.KWNMYE7LuS',
+        role: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: '3f1994bd-6df7-438a-874c-b835687306f7',
+        username: 'test',
+        hash: '$2b$12$hJTbreVMVAci9QHZDe0lSOM90M0z7QIHbR7JBWUxMwFaOM0Vq7JCm',
+        role: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
         id: 'fc16f7d7-c1e2-40e8-95bf-ae35cd4272c9',
         username: 'test2',
         hash: '$2b$12$E0wV3UMvs0NPAsp5/qQG2ehL3Ontkzs.WQaN0AT.S1N..ah4stRS2',
--- src/tests/routes/user.test.js
+++ src/tests/routes/user.test.js
@@ -1,8 +1,11 @@
 process.env.NODE_ENV = 'test';
+process.env.JWT_SECRET = 'ImSuperSecret';
 
 const { describe } = require('mocha');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const jwt = require('jwt-simple');
+
 const api = require('../../routes/routes');
 const { User } = require('../../db/models');
 
@@ -10,40 +13,81 @@ chai.use(chaiHttp);
 chai.should();
 
 describe('Users controller', () => {
+  let adminUser;
+  let testUser;
+  let adminToken;
+  let userToken;
+
+  before(async () => {
+    adminUser = await User.findOne({
+      where: { username: 'admin' },
+    });
+    testUser = await User.findOne({
+      where: { username: 'test' },
+    });
+    adminToken = jwt.encode(
+      { sub: adminUser.id, role: adminUser.role },
+      process.env.JWT_SECRET
+    );
+    userToken = jwt.encode(
+      { sub: testUser.id, role: testUser.role },
+      process.env.JWT_SECRET
+    );
+  });
+
   describe('Get all users', () => {
-    it('Should return a status 200 and an array', async () => {
+    it('Should return a status 403 if no token provided', async () => {
       const response = await chai.request(api).get(`/api/v1/users`);
+      response.should.have.status(403);
+    });
+
+    it('Should return a status 403 if a user token is provided', async () => {
+      const response = await chai
+        .request(api)
+        .get(`/api/v1/users`)
+        .set('Authorization', `Bearer ${userToken}`);
+      response.should.have.status(403);
+    });
+
+    it('Should return a status 200 and an array if an admin token is provided', async () => {
+      const response = await chai
+        .request(api)
+        .get(`/api/v1/users`)
+        .set('Authorization', `Bearer ${adminToken}`);
       response.should.have.status(200);
-      response.body.should.be.an('array');
     });
   });
+
   describe('Create a user', () => {
     before(async () => {
       // Remove test user before tests
       await User.destroy({
         where: {
-          username: 'test',
+          username: 'test2',
         },
       });
     });
+
     it('Should return a status 400 if the JSON schema is not valid', async () => {
-      const invalidUser = { login: 'test', password: 'test' };
+      const invalidUser = { login: 'test2', password: 'test' };
       const response = await chai
         .request(api)
         .post(`/api/v1/users`)
         .send(invalidUser);
       response.should.have.status(400);
     });
+
     it('Should return a status 201 if the JSON schema is valid', async () => {
-      const newUser = { username: 'test', hash: 'imsupersecret' };
+      const newUser = { username: 'test2', hash: 'imsupersecret' };
       const response = await chai
         .request(api)
         .post(`/api/v1/users`)
         .send(newUser);
       response.should.have.status(201);
     });
+
     it('Should return a status 409 if the username is already taken by another user', async () => {
-      const newUser = { username: 'test', hash: '12345678' };
+      const newUser = { username: 'test2', hash: '12345678' };
       const response = await chai
         .request(api)
         .post(`/api/v1/users`)
@@ -51,18 +95,33 @@ describe('Users controller', () => {
       response.should.have.status(409);
     });
   });
+
   describe('Get single user by id', () => {
-    let user;
-    before(async () => {
-      user = await User.findOne({ where: { username: 'test2' } });
-      // eslint-disable-next-line no-unused-expressions
-      chai.expect(user).to.not.be.null;
+    it('Should return a status 403 if no token provided', async () => {
+      const response = await chai
+        .request(api)
+        .get(`/api/v1/users/${testUser.id}`);
+      response.should.have.status(403);
     });
+
+    it('Should return a status 403 if the user to get is not the one who provide the token', async () => {
+      const testUser2 = await User.findOne({ where: { username: 'test2' } });
+      const response = await chai
+        .request(api)
+        .get(`/api/v1/users/${testUser2.id}`)
+        .set('Authorization', `Bearer ${userToken}`);
+      response.should.have.status(403);
+    });
+
     it('Should return a status 200 and the user if user exist', async () => {
-      const response = await chai.request(api).get(`/api/v1/users/${user.id}`);
+      const response = await chai
+        .request(api)
+        .get(`/api/v1/users/${testUser.id}`)
+        .set('Authorization', `Bearer ${userToken}`);
       response.should.have.status(200);
-      response.body.id.should.equal(user.id);
+      response.body.id.should.equal(testUser.id);
     });
+
     it('Should return a status 404 if user does not exist', async () => {
       const nonExistingId = 'e02593fd-8f04-4bd7-915c-fc0e5b034a4a';
       // eslint-disable-next-line no-unused-expressions
@@ -70,7 +129,8 @@ describe('Users controller', () => {
         .null;
       const response = await chai
         .request(api)
-        .get(`/api/v1/users/${nonExistingId}`);
+        .get(`/api/v1/users/${nonExistingId}`)
+        .set('Authorization', `Bearer ${adminToken}`);
       response.should.have.status(404);
     });
   });
