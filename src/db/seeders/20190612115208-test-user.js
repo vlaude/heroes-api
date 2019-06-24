ab0466be9e443b36821e8624d1e32ad6550196ea
Vincent Laude <vincent.laude2@gmail.com>
2019-06-24 11:40:42 +0200

    bugfix: role in seed user test

--- src/db/seeders/20190612115208-test-user.js
+++ src/db/seeders/20190612115208-test-user.js
@@ -5,7 +5,7 @@ module.exports = {
         id: 'fc16f7d7-c1e2-40e8-95bf-ae35cd4272c9',
         username: 'test2',
         hash: '$2b$12$E0wV3UMvs0NPAsp5/qQG2ehL3Ontkzs.WQaN0AT.S1N..ah4stRS2',
-        role: 'USER',
+        role: 1,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
