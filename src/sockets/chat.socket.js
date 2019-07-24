e85134a9d680e2d37d2d68c0e74c825088ce14e0
Vincent Laude <vincent.laude2@gmail.com>
2019-07-24 17:00:49 +0200

    WIP : handle chat connection

--- src/sockets/chat.socket.js
+++ src/sockets/chat.socket.js
@@ -3,18 +3,22 @@ const userBuilder = require('../builders/user.builder');
 const roomBuilder = require('../builders/room.builder');
 
 const initChat = io => {
+    const clients = {};
+
     io.of('/chat').on('connection', socket => {
-        console.log('a user is connected to the chat');
+        const username = socket.handshake.query.username;
+        console.log(`${username} is connected to the chat`);
+
+        clients[username] = socket.id;
+        io.of('/chat').emit('clients', clients);
 
         socket.on('join-room', room => {
             socket.join(room);
             console.log(`a user join room ${room}`);
 
             socket.on('new-message', async msg => {
-                console.log(`message sent to room ${room} : ${msg.message}`);
                 socket.in(room).emit('new-message', msg);
 
-                // TODO Look at this
                 if (msg.room.name === room) {
                     const poster = await userBuilder.getUserById(msg.poster.id);
                     const roomEntity = await roomBuilder.getRoomById(msg.room.id);
@@ -26,6 +30,12 @@ const initChat = io => {
         socket.on('leave-room', room => {
             socket.leave(room);
             console.log(`a user leave room ${room}`);
+        });
+
+        socket.on('disconnect', () => {
+            console.log(`${username} is now disconnected`);
+            delete clients[username];
+            io.of('/chat').emit('clients', clients);
         });
     });
 };
